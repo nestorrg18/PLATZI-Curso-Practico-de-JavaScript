deb43d412bbdc8bfe8a42fd26eb85fa1eda4117a
nestorrg18 <neriga10@hotmail.com>
2023-02-01 22:12:25 -0600

    Clase 11: Escribiendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ console.log({
     pall,
     input
 });
+/// 11. escribiendo HTML desde JS
+
+h1.innerHTML = "NO <br> Cool" // este no es recomendable por temas de seguridad
+h1.innerText = "Más cool"
+// obteniendo y modificando atributos
+let atributo = h1.getAttribute("pantalla")
+console.log(h1.getAttribute("class"));
+h1.setAttribute("class", "rojo")
+console.log(atributo);
+// para clases
+h1.classList.add("azul")
+h1.classList.remove("rojo")
+// h1.classList.toggle("rojo") || util para eventos
+// h1.classList.contains("rojo") || devuelve true o false
+input.value = "456"
+// creando y agregando html desde 0
+imagen = document.createElement("img")
+imagen.setAttribute("src", "https://static.platzi.com/media/achievements/practico-javascript_badge-fb533fa5-73c8-4f75-9d81-0294d9013ab5.png")
+console.log(imagen);
+pid.innerText = ""
+pid.appendChild(imagen)
+//pid.replaceWith(imagen); tambien hacer esto y olvidarte de las 2 lineas anteriores
